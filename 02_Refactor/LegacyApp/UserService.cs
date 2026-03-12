1d4d462f4d3bccd9c92f6021d36aada73e4a3706
kacpibara <kacperbarancewicz@gmail.com>
2026-03-12 16:16:46 +0100

    Task 2: Refactor: Extract age calculation to CalculateAge method

--- 02_Refactor/LegacyApp/UserService.cs
+++ 02_Refactor/LegacyApp/UserService.cs
@@ -11,10 +11,7 @@ namespace LegacyApp
                 return false;
             }
             
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
+            int age = CalculateAge(dateOfBirth);
             if (age < 21)
             {
                 return false;
@@ -77,6 +74,18 @@ namespace LegacyApp
             }
 
             return true;
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+            {
+                age--;
+            }
+
+            return age;
         }
     }
     
